262e05564635380bf5a86513ba41a2e2619399db
Adam Dinan <adam1989ie@gmail.com>
2023-04-06 11:45:24 +0100

    Add event listeners for buttons

--- script.js
+++ script.js
@@ -20,3 +20,26 @@ function createGrid(size) {
 }
 
 createGrid(gridSize);
+
+clearBtn.addEventListener('click', () => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.style.backgroundColor = 'white');
+});
+
+resetBtn.addEventListener('click', () => {
+  const squares = document.querySelectorAll('.square');
+  squares.forEach(square => square.remove());
+  createGrid(gridSize);
+});
+
+changeBtn.addEventListener('click', () => {
+  let size = prompt('Enter new grid size (1-100):');
+  if (size === null || size === '' || isNaN(size) || size < 1 || size > 100) {
+    alert('Invalid input! Please enter a number between 1 and 100.');
+  } else {
+    gridSize = parseInt(size);
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.remove());
+    createGrid(gridSize);
+  }
+});
