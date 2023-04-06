e051b12a23db3a6e5263a490d0f0fc13ac819f6b
Adam Dinan <adam1989ie@gmail.com>
2023-04-06 16:17:36 +0100

    Make slider return to default when Reset button clicked

--- script.js
+++ script.js
@@ -18,6 +18,9 @@ resetBtn.addEventListener('click', resetGrid);
 
 
 function createGrid(size) {
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
   gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   gridContainer.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   for (let i = 0; i < size * size; i++) {
@@ -54,6 +57,8 @@ function resetGrid()  {
   const squares = document.querySelectorAll('.square');
   squares.forEach(square => square.remove());
   let gridSize = DEFAULT_SIZE;
+  slider.value = DEFAULT_SIZE;
+  sliderValue.textContent = DEFAULT_SIZE;
   createGrid(gridSize);
 }
 
